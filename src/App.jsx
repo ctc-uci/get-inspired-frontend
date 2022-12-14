6a801c1f135cc0912fc25f8cd4d86441f96d4820
Kory Zhang <koryiskool@yahoo.com>
2022-12-13 16:39:24 -0800

    Created basic frontend structure (routes, login + home page)

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
   );
 }
 
