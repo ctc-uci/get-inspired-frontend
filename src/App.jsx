1d2b77cbb8bd32f0046a1246026ae48c36a61dd0
Ted Kim <kimgeobuk@gmail.com>
2023-01-26 10:46:32 -0800

    creating branch

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
+import ManageUsersPage from './pages/ManageUsers';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/manage-users" element={<ManageUsersPage />} />
       </Routes>
     </Router>
   );
