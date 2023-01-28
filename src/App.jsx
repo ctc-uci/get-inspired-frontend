e589e3f7cde7152272c8298d48957c9b3385278b
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2023-01-27 17:37:41 -0800

    Add Frontend Routes (#21)
    + fix yarn dev dependencies
    ---------
    Co-authored-by: Subin Kim <subinlk@uci.edu>
    Co-authored-by: Alan Chang <alan@alanchang.xyz>

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import LoginPage from './pages/LoginPage';
 import './App.css';
+import Dashboard from './pages/Dashboard/Dashboard';
+import Login from './pages/Login/Login';
+import Profile from './pages/Profile/Profile';
+import Surveys from './pages/Surveys/Surveys';
+import Data from './pages/Data/Data';
+import AddData from './pages/AddData/AddData';
+import ImportCSV from './pages/ImportCSV/ImportCSV';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<HomePage />} />
-        <Route exact path="/login" element={<LoginPage />} />
-      </Routes>
-    </Router>
+    <React.StrictMode>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route exact path="/profile" element={<Profile />} />
+          <Route exact path="/import-csv" element={<ImportCSV />} />
+          <Route exact path="/surveys" element={<Surveys />} />
+          <Route exact path="/data" element={<Data />} />
+          <Route exact path="/data/add" element={<AddData />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </React.StrictMode>
   );
 }
 
---
+++ src/pages/ImportCSV/ImportCSV.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ImportCSV = () => {
+  return <h1>Import Csv Pascal</h1>;
+};
+
+export default ImportCSV;
