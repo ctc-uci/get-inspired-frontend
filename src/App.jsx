21e0325b115507ef7af033bc41d0247352843f8d
subinqkim <subinlk@uci.edu>
2023-01-27 16:31:40 -0800

    Removed stray HomePage path

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
-import LoginPage from './pages/Login/LoginPage';
+import LoginPage from './pages/LoginPage/LoginPage';
 import './App.css';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Profile from './pages/Profile/Profile';
@@ -16,11 +15,10 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Dashboard />} />
-          <Route exact path="/homepage" element={<HomePage />} />
           <Route exact path="/profile" element={<Profile />} />
-          <Route exact path="/importcsv" element={<ImportCSV />} />
+          <Route exact path="/import-csv" element={<ImportCSV />} />
           <Route exact path="/surveys" element={<Surveys />} />
-          <Route exact path="/adddata" element={<AddData />} />
+          <Route exact path="/data/add" element={<AddData />} />
           <Route exact path="/data" element={<Data />} />
           <Route exact path="/login" element={<LoginPage />} />
         </Routes>
