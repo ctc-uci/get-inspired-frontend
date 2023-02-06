0537d64edd42a978fcda21dbe731e6ec39861baa
ctc-devops <90984711+ctc-devops@users.noreply.github.com>
2023-02-05 16:47:21 -0800

    Authentication With Login and Register (#34)
    * Avent and I got started on the login, logout, register, and create new user. Got stuck on creating the user routes.
    
    * Worked with Claude to finish register/login user functionality
    
    * Finished Frontend Login
    
    * Made modal for register
    
    * Register page and setup for ManageUser
    
    * Add User Modal CSS
    
    * Radio group value now defaults to viewer. Previously it was passing undefined as the role instead of viewer.
    
    * Added the login redirect to dashboard for logged-in users logic to Login.jsx
    
    * Removed package-lock.json
    
    * Reviewed auth
    
    * Changed /user to /users in auth_utils
    
    ---------
    
    Co-authored-by: Jacqueline Thai <jthai1201@gmail.com>
    Co-authored-by: Avent Chiu <chiual@uci.edu>

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,150 @@
+import React, { useState } from 'react';
+import { Button, Form, Input, Radio, Typography } from 'antd';
+import { instanceOf } from 'prop-types';
+import { useNavigate } from 'react-router-dom';
+import { Cookies, withCookies } from '../../utils/cookie_utils';
+import { registerWithEmailAndPassword } from '../../utils/auth_utils';
+
+import GSPLogo from '../../assets/GSPLogo.svg';
+import styles from './Register.module.css';
+
+const { Title } = Typography;
+
+// eslint-disable-next-line no-unused-vars
+const Register = ({ cookies }) => {
+  const [errorMessage, setErrorMessage] = useState();
+  const navigate = useNavigate();
+
+  const handleSubmit = async values => {
+    try {
+      const { role, firstName, lastName, email, password, checkPassword } = values;
+      if (password !== checkPassword) {
+        throw new Error("Passwords don't match");
+      }
+
+      await registerWithEmailAndPassword(
+        email,
+        password,
+        role,
+        firstName,
+        lastName,
+        navigate,
+        '/login',
+      );
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+
+  return (
+    <>
+      <div className={styles['logo-container']}>
+        <img src={GSPLogo} alt="Get Inspired Logo" />
+        <h1>
+          Get Inspired: <br /> Pismo Clam Database
+        </h1>
+      </div>
+      <div className={styles.container}>
+        <Title>Sign Up</Title>
+        <Form
+          id="login-form"
+          layout="vertical"
+          name="login-form"
+          onFinish={handleSubmit}
+          className={styles['register-form']}
+          initialValues={{ role: 'viewer' }}
+        >
+          <span>
+            <Form.Item
+              id="roles"
+              label=""
+              name="role"
+              rules={[
+                {
+                  required: true,
+                  message: 'Please choose a role!',
+                },
+              ]}
+            >
+              <Radio.Group defaultValue="viewer">
+                <Radio value="viewer">Viewer</Radio>
+                <Radio value="editor">Editor</Radio>
+              </Radio.Group>
+            </Form.Item>
+          </span>
+
+          <Form.Item
+            label="First Name"
+            name="firstName"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your first name!',
+              },
+            ]}
+          >
+            <Input type="text" />
+          </Form.Item>
+          <Form.Item
+            label="Last Name"
+            name="lastName"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your last name!',
+              },
+            ]}
+          >
+            <Input type="text" />
+          </Form.Item>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your email!',
+              },
+            ]}
+          >
+            <Input type="email" />
+          </Form.Item>
+          <Form.Item
+            label="Set Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password type="text" />
+          </Form.Item>
+          <Form.Item
+            label="Confirm Password"
+            name="checkPassword"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password type="text" />
+          </Form.Item>
+        </Form>
+        <p>{errorMessage}</p>
+        <Button type="primary" form="login-form" key="submit" htmlType="submit">
+          Submit
+        </Button>
+      </div>
+    </>
+  );
+};
+
+Register.propTypes = {
+  cookies: instanceOf(Cookies).isRequired,
+};
+
+export default withCookies(Register);
---
+++ src/pages/Register/RegisterPage.jsx
@@ -0,0 +1,12 @@
+import { React } from 'react';
+import Register from '../../components/Register/Register';
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <Register />
+    </div>
+  );
+};
+
+export default RegisterPage;
