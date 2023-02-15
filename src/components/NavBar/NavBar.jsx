3f2330bb8a03423cc9e339dfceedac1befe970f1
subinqkim <subinlk@uci.edu>
2023-02-15 14:45:56 -0800

    better description for keys

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -19,22 +19,22 @@ const VerticalMenu = () => {
   return (
     <Sider width={SIDER_WIDTH}>
       <Menu mode="inline" defaultSelectedKeys={['1']} style={{ height: '100%', borderRight: 0 }}>
-        <Menu.Item key="1" icon={<GlobalOutlined />}>
+        <Menu.Item key="global-log" icon={<GlobalOutlined />}>
           GetInspired
         </Menu.Item>
-        <Menu.Item key="2" icon={<HomeFilled />}>
+        <Menu.Item key="home-logo" icon={<HomeFilled />}>
           <Link to="/Dashboard">Dashboard</Link>
         </Menu.Item>
-        <Menu.Item key="3" icon={<DatabaseFilled />}>
+        <Menu.Item key="database-logo" icon={<DatabaseFilled />}>
           <Link to="/Data">View Data</Link>
         </Menu.Item>
-        <Menu.Item key="4" icon={<SafetyCertificateFilled />}>
+        <Menu.Item key="certificate-logo" icon={<SafetyCertificateFilled />}>
           <Link to="/Data/Add">Add Data</Link>
         </Menu.Item>
-        <Menu.Item key="5" icon={<ClockCircleFilled />}>
+        <Menu.Item key="clock-logo" icon={<ClockCircleFilled />}>
           Query Data
         </Menu.Item>
-        <Menu.Item key="6" icon={<BellFilled />}>
+        <Menu.Item key="bell-logo" icon={<BellFilled />}>
           Notifications
         </Menu.Item>
       </Menu>
