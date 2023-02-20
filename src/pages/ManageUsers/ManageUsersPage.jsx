08f7236b534f3051bd2d9ffad86424c7a7109f08
Ted Kim <kimgeobuk@gmail.com>
2023-02-19 16:08:56 -0800

    added table and stylyzing

--- src/pages/ManageUsers/ManageUsersPage.jsx
+++ src/pages/ManageUsers/ManageUsersPage.jsx
@@ -1,10 +1,79 @@
 import { React } from 'react';
+import { Table, Space } from 'antd';
 import ManageUsers from '../../components/ManageUsers/ManageUsers';
+
+const { Column } = Table;
+
+const data = [
+  {
+    name: 'John',
+    email: 'john@uci.edu',
+    password: 'testing',
+    role: 'dev',
+    key: '1',
+  },
+  {
+    name: 'Ted',
+    email: 'ted@uci.edu',
+    key: '2',
+  },
+  {
+    name: 'Matt',
+    email: 'matt@gmail.com',
+    key: '3',
+  },
+];
+
+/*
+will fix this later (add notification to the import antd)
+const [api, contextHolder] = notification.useNotification();
+const openNotification = (placement) => { // call this function after form submitted
+  api.info({
+    message: `Notification ${placement}`,
+    description:
+      'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+    placement,
+  });
+};
+*/
 
 const ManageUsersPage = () => {
   return (
     <div>
-      <ManageUsers />
+      <div id="wrapper">
+        <div id="div1">
+          <h1>Manage Users</h1>
+        </div>
+        <div id="div2">
+          <ManageUsers />
+        </div>
+      </div>
+      <Table dataSource={data}>
+        <Column title="Name" dataIndex="name" key="name" />
+        <Column title="Email" dataIndex="email" key="email" />
+        <Column title="Password" dataIndex="password" key="password" />
+        <Column title="Role" dataIndex="role" key="role" />
+        <Column
+          title="Account History"
+          dataIndex="history"
+          key="history"
+          render={() => (
+            <Space size="middle">
+              <a href="https://www.youtube.com/watch?v=pSUydWEqKwE">View</a>
+            </Space>
+          )}
+        />
+        <Column
+          title="Setting"
+          key="setting"
+          render={() => (
+            <Space size="middle">
+              <a href="https://www.youtube.com/watch?v=pSUydWEqKwE">Edit</a>
+              <a href="https://www.youtube.com/watch?v=pSUydWEqKwE">Delete</a>
+            </Space>
+          )}
+        />
+      </Table>
     </div>
   );
 };
