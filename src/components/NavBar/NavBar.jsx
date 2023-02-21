ea71e9967e3e3708d2270b1e5bfdb4f385a28577
subinqkim <subinlk@uci.edu>
2023-02-20 21:49:03 -0800

    combining keys into one array

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Layout, Menu } from 'antd';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import {
   ClockCircleFilled,
@@ -8,18 +8,32 @@ import {
   SafetyCertificateFilled,
   DatabaseFilled,
   BellFilled,
-  GlobalOutlined,
 } from '@ant-design/icons';
+
+import { ReactComponent as GSPLogo } from '../../assets/GSPLogo.svg';
 
 const SIDER_WIDTH = 200;
 
 const { Sider } = Layout;
 
 const VerticalMenu = () => {
+  const location = useLocation();
+  const path = location.pathname;
+
+  const selectedKeys = [
+    ...(path === '/data' ? ['database-logo'] : []),
+    ...(path === '/' ? ['certificate-logo'] : []),
+    ...(path === '/Dashboard' ? ['home-logo'] : []),
+    ...(path === '/data' ? ['clock-logo'] : []),
+  ];
   return (
     <Sider width={SIDER_WIDTH} collapsible>
-      <Menu mode="inline" defaultSelectedKeys={['1']} style={{ height: '100%', borderRight: 0 }}>
-        <Menu.Item key="global-log" icon={<GlobalOutlined />}>
+      <Menu
+        mode="inline"
+        defaultSelectedKeys={selectedKeys}
+        style={{ height: '100%', borderRight: 0 }}
+      >
+        <Menu.Item key="global-log" icon={<GSPLogo />}>
           GetInspired
         </Menu.Item>
         <Menu.Item key="home-logo" icon={<HomeFilled />}>
