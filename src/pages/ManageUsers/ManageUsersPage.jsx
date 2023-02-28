bcab8824564acf2e3aeb3e00d4c5257a03bf5379
claudeisakeeb <claudeyan2001@gmail.com>
2023-02-27 17:19:38 -0800

    Remove AddUserModal from ManageUsersPage

--- src/pages/ManageUsers/ManageUsersPage.jsx
+++ src/pages/ManageUsers/ManageUsersPage.jsx
@@ -1,14 +1,8 @@
 import React from 'react';
-import AddUserModal from '../../components/ManageUsers/AddUserModal/AddUserModal';
 import ManageUsers from '../../components/ManageUsers/ManageUsers';
 
 const ManageUsersPage = () => {
-  return (
-    <>
-      <AddUserModal />
-      <ManageUsers />
-    </>
-  );
+  return <ManageUsers />;
 };
 
 export default ManageUsersPage;
