8d5d02dc16cedfbae2a5c50725d05848bc02f95f
Kory Zhang <koryiskool@yahoo.com>
2023-03-09 21:05:38 -0800

    Setup Add Data To Existing Survey

--- src/components/AddData/StepsBar/StepsBar.jsx
+++ src/components/AddData/StepsBar/StepsBar.jsx
@@ -2,37 +2,22 @@ import { React } from 'react';
 import { Steps } from 'antd';
 import PropTypes from 'prop-types';
 
-function StepsBar({ curStep }) {
+function StepsBar({ curStep, titleArray }) {
   return (
     <div>
-      <Steps
-        size="small"
-        current={curStep}
-        items={[
-          {
-            title: 'Survey',
-          },
-          {
-            title: 'Clams',
-          },
-          {
-            title: 'Raker',
-          },
-          {
-            title: 'Review',
-          },
-        ]}
-      />
+      <Steps size="small" current={curStep} items={titleArray} />
     </div>
   );
 }
 
 StepsBar.defaultProps = {
   curStep: PropTypes.number,
+  titleArray: PropTypes.arrayOf(PropTypes.shape({ title: PropTypes.string })),
 };
 
 StepsBar.propTypes = {
   curStep: PropTypes.number,
+  titleArray: PropTypes.arrayOf(PropTypes.shape({ title: PropTypes.string })),
 };
 
 export default StepsBar;
--- src/pages/AddData/AddData.jsx
+++ src/pages/AddData/AddData.jsx
@@ -11,12 +11,17 @@ import styles from './AddData.module.css';
 const AddData = () => {
   const [curStep, setCurStep] = useState(0);
   const [showChooseSurveyButtons, setShowChooseSurveyButtons] = useState(true);
+  const [showNewSurvey, setShowNewSurvey] = useState(false);
   const [showExistingSurvey, setShowExistingSurvey] = useState(false);
 
-  const handleCreateNewSurvey = useCallback(() => {
+  const handleCreateNewSurvey = () => {
+    setShowChooseSurveyButtons(false);
+    setShowNewSurvey(true);
+  };
+  const handleAddToExistingSurvey = () => {
     setShowChooseSurveyButtons(false);
     setShowExistingSurvey(true);
-  });
+  };
   const incrStep = useCallback(() => {
     setCurStep(prevStep => {
       return prevStep + 1;
@@ -35,7 +40,12 @@ const AddData = () => {
         {showChooseSurveyButtons && (
           <div className={styles.chooseSurveyBtnsDiv}>
             {showChooseSurveyButtons && (
-              <Button className={styles.chooseSurveyBtns} type="primary" icon={<EditOutlined />}>
+              <Button
+                className={styles.chooseSurveyBtns}
+                type="primary"
+                icon={<EditOutlined />}
+                onClick={() => handleAddToExistingSurvey()}
+              >
                 Add to existing survey
               </Button>
             )}
@@ -51,19 +61,33 @@ const AddData = () => {
             )}
           </div>
         )}
-        {showExistingSurvey && curStep < 4 && <StepsBar curStep={curStep} />}
+        {showNewSurvey && curStep < 4 && (
+          <StepsBar
+            curStep={curStep}
+            titleArray={[
+              { title: 'Survey' },
+              { title: 'Clams' },
+              { title: 'Raker' },
+              { title: 'Review' },
+            ]}
+          />
+        )}
+        {showExistingSurvey && curStep < 3 && (
+          <StepsBar
+            curStep={curStep}
+            titleArray={[{ title: 'Select' }, { title: 'Upload' }, { title: 'Preview' }]}
+          />
+        )}
       </div>
-      {showExistingSurvey && curStep === 0 && <SurveyForm incrStep={incrStep} />}
-      {showExistingSurvey && curStep === 1 && (
+      {showNewSurvey && curStep === 0 && <SurveyForm incrStep={incrStep} />}
+      {showNewSurvey && curStep === 1 && (
         <ImportCSV incrStep={incrStep} decrStep={decrStep} typeOfData="Clams" />
       )}
-      {showExistingSurvey && curStep === 2 && (
+      {showNewSurvey && curStep === 2 && (
         <ImportCSV incrStep={incrStep} decrStep={decrStep} typeOfData="Raker" />
       )}
-      {showExistingSurvey && curStep === 3 && (
-        <ReviewForm incrStep={incrStep} decrStep={decrStep} />
-      )}
-      {showExistingSurvey && curStep === 4 && <UploadComplete />}
+      {showNewSurvey && curStep === 3 && <ReviewForm incrStep={incrStep} decrStep={decrStep} />}
+      {showNewSurvey && curStep === 4 && <UploadComplete />}
     </div>
   );
 };
