d3dd386cabb112e28595c87b9ffed2478bb65dc6
claudeisakeeb <claudeyan2001@gmail.com>
2023-03-14 16:36:50 -0700

    Add data on submit ReviewForm

--- src/components/AddData/ReviewForm/ReviewForm.jsx
+++ src/components/AddData/ReviewForm/ReviewForm.jsx
@@ -2,12 +2,32 @@ import { React } from 'react';
 import { CaretDownOutlined } from '@ant-design/icons';
 import { Row, Col, Button, Collapse, theme, Table } from 'antd';
 import PropTypes from 'prop-types';
+
+import { GSPBackend } from '../../../utils/utils';
+
 import styles from './ReviewForm.module.css';
 
 const { Panel } = Collapse;
 
-function ReviewForm({ incrStep, decrStep, csvData }) {
+function ReviewForm({ incrStep, decrStep, surveyData, csvData }) {
   const { token } = theme.useToken();
+
+  const addData = async () => {
+    const surveyId = (await GSPBackend.post('/surveys', surveyData)).data[0].insertId;
+
+    const addClamAndRakerRequests = [
+      ...(csvData.clam
+        ? csvData.clam.map(clamData => GSPBackend.post('/clams', { surveyId, ...clamData }))
+        : []),
+      ...(csvData.raker
+        ? csvData.raker.map(rakerData => GSPBackend.post('/rakers', { surveyId, ...rakerData }))
+        : []),
+    ];
+
+    await Promise.all(addClamAndRakerRequests);
+
+    incrStep();
+  };
 
   const panelStyle = {
     marginBottom: 24,
@@ -52,7 +72,7 @@ function ReviewForm({ incrStep, decrStep, csvData }) {
           </Button>
         </Col>
         <Col span={12}>
-          <Button type="primary" onClick={incrStep}>
+          <Button type="primary" onClick={addData}>
             Submit
           </Button>
         </Col>
@@ -69,6 +89,7 @@ ReviewForm.defaultProps = {
 ReviewForm.propTypes = {
   incrStep: PropTypes.func,
   decrStep: PropTypes.func,
+  surveyData: PropTypes.shape({}).isRequired,
   csvData: PropTypes.shape({
     clam: PropTypes.arrayOf(PropTypes.shape({})),
     raker: PropTypes.arrayOf(PropTypes.shape({})),
