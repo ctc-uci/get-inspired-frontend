32dc713054cbac0b088d86cc7441dfbc10cdaba5
subinqkim <subinlk@uci.edu>
2023-04-02 16:40:15 -0700

    added chaining operator to ensure that user is accessing obj properties when currentUser obj is not null

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -9,7 +9,6 @@ const auth1 = auth;
 
 const Profile = () => {
   const [currentUser, setCurrentUser] = useState(null);
-
   useEffect(() => {
     const fetchUser = async () => {
       if (auth1) {
@@ -22,14 +21,13 @@ const Profile = () => {
     };
     fetchUser();
   }, [auth]);
-
   return (
     <div>
       <Title level={2}>Profile Page</Title>
-      <p>First Name: {currentUser[0].firstName}</p>
-      <p>Last Name: {currentUser[0].lastName}</p>
-      <p>Email: {currentUser[0].email}</p>
-      <p>Role: {currentUser[0].role}</p>
+      <p>First Name: {currentUser?.[0]?.firstName}</p>
+      <p>Last Name: {currentUser?.[0]?.lastName}</p>
+      <p>Email: {currentUser?.[0]?.email}</p>
+      <p>Role: {currentUser?.[0]?.role}</p>
     </div>
   );
 };
